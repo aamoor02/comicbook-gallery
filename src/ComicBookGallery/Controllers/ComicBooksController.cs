0df2db2c8eaa3e6240ee229c8fe30bc0b16da14f
Aaron Moorman <amoorman@gmail.com>
2018-07-07 17:28:00 -0400

    update view and controller

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,6 +11,20 @@ namespace ComicBookGallery.Controllers
         //adding an action method
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "the amazing Spider-man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "< p > Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+                {
+                    "Script: Dan Slott",
+                    "Pencils: Humberto Ramos",
+                    "Inks: Victor Olazaba",
+                    "Colors: Edgar Delgado",
+                    "Letters: Chris Eliopoulos"
+                };
+
+
+
             return View();
            
             
